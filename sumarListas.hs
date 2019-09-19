92ac8ba2f452f348281b241e0497bbb80647e0ba
Rafael Ricardo Uribe Perdomo <42448590+ZidTri@users.noreply.github.com>
2019-09-19 13:42:32 -0500

    Update sumarListas.hs

--- sumarListas.hs
+++ sumarListas.hs
@@ -1,6 +1,4 @@
 
 sumarLista::[Int]->[Int]->[Int]
-sumarLista [] [] = []
-sumarLista (x:xs) [] = (x) : (sumarLista xs [])
-sumarLista [] (y:ys) = (y) : (sumarLista [] ys)
+sumarLista [] li = []
 sumarLista (x:xs) (y:ys) = (x+y) : (sumarLista xs ys)
